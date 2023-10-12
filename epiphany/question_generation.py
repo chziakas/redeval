a9fcd1e14c55a0a6cdf96f8ebd320c3c39bfd8e0
Christos <chziakas@gmail.com>
2023-10-11 22:04:35 -0700

    fix: add precommits

--- epiphany/question_generation.py
+++ epiphany/question_generation.py
@@ -1,12 +1,7 @@
-import os
 from dotenv import load_dotenv
-from llama_index import SimpleDirectoryReader, ServiceContext
-
+from llama_index import ServiceContext, SimpleDirectoryReader
 from llama_index.evaluation import DatasetGenerator
-
 from llama_index.llms import OpenAI
-
-
 
 load_dotenv()
 
@@ -14,22 +9,24 @@ load_dotenv()
 gpt4 = OpenAI(temperature=0, model="gpt-4")
 service_context_gpt4 = ServiceContext.from_defaults(llm=gpt4)
 
+
 def generate_questions(num_questions, directory_path):
     reader = SimpleDirectoryReader(directory_path)
     documents = reader.load_data()
     data_generator = DatasetGenerator.from_documents(documents)
-    
+
     # Generate questions
     eval_questions = data_generator.generate_questions_from_nodes(num=num_questions)
-    
+
     # You can add any 'bad' or additional questions here
-    bad_questions = ['What is the weather in SF?']
-    
+    bad_questions = ["What is the weather in SF?"]
+
     # Combine the lists
     questions_list = eval_questions + bad_questions
-    
+
     return questions_list
 
+
 # Example use:
-questions = generate_questions(num_questions = 2, directory_path = 'data/examples/rag')
+questions = generate_questions(num_questions=2, directory_path="data/examples/rag")
 print(questions)
