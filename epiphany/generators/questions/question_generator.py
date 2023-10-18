bfc6d8d1fdfe1f5eb367359d8778abe3f089f365
Christos <chziakas@gmail.com>
2023-10-17 23:31:45 -0700

    feat: add rag simulator

---
+++ epiphany/generators/questions/question_generator.py
@@ -0,0 +1,49 @@
+from epiphany.evaluators.rag.open_ai_completion import OpenAICompletion
+
+
+class ConversationalGenerator:
+    """
+    This class generates a follow-up question
+
+    Attributes:
+        openAIcompletion (OpenAICompletion): Instance for interactions with OpenAI's API.
+        examples (list[FewShotExampleFaithfulness]): List of few-shot examples used for evaluation.
+    """
+
+    SYSTEM_MESSAGE = """
+        You are an expert at simulating a conversation between a human and a chatbot.
+    """
+
+    USER_MESSAGE_TEMPLATE = """
+        Let's think step by step.
+        1. Consider the following:
+        previous conversation between human and chatbot {}.
+        chatbot's last answer: {}.
+        2. Generate a question to continue a natural converastion between the chatbot's last answe, labeled as 'question', based on the chatbot's answer and previous conversation.
+        3. Return a JSON object in the following format: "question": 'question.
+    """
+
+    def __init__(self, model, open_ai_key):
+        """
+        Initialize the QuestionAnswerer class.
+        """
+        self.openAIcompletion = OpenAICompletion(model, open_ai_key)
+        self.memory = ""
+
+    def generate(self, query: str, answer: str):
+        """
+        Evaluation for is response faithful to context
+        """
+        self.memory += "Human: " + query + "\n"
+        user_message = self.USER_MESSAGE_TEMPLATE.format(self.memory, answer)
+        system_message = self.SYSTEM_MESSAGE
+        message = [
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": user_message},
+        ]
+
+        openai_response = self.openAIcompletion.get_completion_from_messages(message)
+        openai_response_json = self.openAIcompletion.extract_json_from_response(openai_response)
+        question = openai_response_json["question"]
+        self.memory += "Chatbot: " + answer + "\n"
+        return question
--- rag_simulator.py
+++ rag_simulator.py
@@ -9,6 +9,7 @@ from epiphany.evaluators.rag.answer_relevance import AnswerRelevance
 from epiphany.evaluators.rag.context_relevance import ContextRelevance
 from epiphany.evaluators.rag.faithfulness import Faithfulness
 from epiphany.generators.questions.lamma_index_generator import QuestionGenerator
+from epiphany.generators.questions.question_generator import ConversationalGenerator
 
 # Load environment variables from a .env file (if it exists)
 load_dotenv()
@@ -22,56 +23,55 @@ generator = QuestionGenerator(openai_api_key=openai_api_key)
 # Generate questions using the generator
 # This includes fetching 2 questions from a directory and appending one manually specified question
 questions = generator.generate_questions(
-    num_questions=2,
+    num_questions=1,
     directory_path="data/examples/rag",
-    additional_questions=["Tell me a bad joke."],
+    additional_questions=[],
 )
-
 
 # Initialize the RAG (Retrieval-augmented generation) Agent with a specific model
 agent = RagAgent("data/examples/rag", openai_api_key=openai_api_key, model_name="gpt-3.5-turbo-16k")
 
-# Fetch contexts for the questions using the RAG Agent
-contexts = agent.get_contexts(questions)
+convo_generation = ConversationalGenerator(model="gpt-3.5-turbo-16k", open_ai_key=openai_api_key)
 
-# Fetch responses for the questions using the RAG Agent
-responses = agent.get_responses(questions)
+context_relevance = ContextRelevance("gpt-3.5-turbo", openai_api_key)
+answer_relevance = AnswerRelevance("gpt-3.5-turbo", openai_api_key)
+faithfulness = Faithfulness("gpt-3.5-turbo", openai_api_key)
 
-
-# Create a list to store each set of data as a dictionary
 data_list = []
 
-# Iterate over each question, context, and response
-for question, context, response in zip(questions, contexts, responses):
-    print(f"({question},{context}, {response})")
+for i in range(5):
+    # Fetch contexts for the questions using the RAG Agent
+    contexts = agent.get_contexts(questions)
+    # Fetch responses for the questions using the RAG Agent
+    responses = agent.get_responses(questions)
 
-    # Evaluate context relevance for the question-context pair
-    context_relevance = ContextRelevance("gpt-3.5-turbo", openai_api_key)
+    answer = convo_generation.generate(questions[0], responses[0])
+    print(answer)
+
     context_relevance_failure, context_relevance_explanation = context_relevance.evaluate(
-        question, context
+        questions[0], contexts[0]
     )
     print(f"context_relevance_failure:{context_relevance_failure}")
     print(f"context_relevance_explanation:{context_relevance_explanation}")
 
     # Evaluate answer relevance for the question-response pair
-    answer_relevance = AnswerRelevance("gpt-3.5-turbo", openai_api_key)
     answer_relevance_failure, answer_relevance_explanation = answer_relevance.evaluate(
-        question, response
+        questions[0], responses[0]
     )
     print(f"answer_relevance_failure:{answer_relevance_failure}")
     print(f"answer_relevance_explanation:{answer_relevance_explanation}")
 
     # Evaluate faithfulness of the response to the context
-    faithfulness = Faithfulness("gpt-3.5-turbo", openai_api_key)
-    faithfulness_failure, faithfulness_explanation = faithfulness.evaluate(context, response)
+    faithfulness_failure, faithfulness_explanation = faithfulness.evaluate(
+        contexts[0], responses[0]
+    )
     print(f"faithfulness_failure:{faithfulness_failure}")
     print(f"faithfulness_explanation:{faithfulness_explanation}")
 
-    # Create a dictionary for the current set of data
     data_dict = {
-        "question": question,
-        "context": context,
-        "response": response,
+        "question": questions[0],
+        "context": contexts[0],
+        "response": responses[0],
         "context_relevance_failure": context_relevance_failure,
         "context_relevance_explanation": context_relevance_explanation,
         "answer_relevance_failure": answer_relevance_failure,
@@ -82,8 +82,8 @@ for question, context, response in zip(questions, contexts, responses):
 
     # Append the dictionary to the list
     data_list.append(data_dict)
-
+    questions = [answer]
 
 # write it to a JSON file
-with open("data/examples/rag/results.json", "w") as file:
+with open("data/examples/rag/convo_results.json", "w") as file:
     json.dump(data_list, file, indent=4)
